1aa5501735fd2bf80c22cb95a3f124e66d6074ad
m-sabid <sabidofficial@gmail.com>
2023-05-03 16:00:27 +0600

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const chefsData = require("./data/chefs.json");
 
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.get("/api/chefs", (req, res) => {
   res.json(chefsData);
